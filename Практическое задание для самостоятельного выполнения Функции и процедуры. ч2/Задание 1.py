54a0fbaa96e0303b8b8229a4a1389bcd0e4cc13a
ShiraSchool18 <145564330+ShiraSchool18@users.noreply.github.com>
2023-11-24 09:22:07 +0000

    Update Задание 1.py

--- Практическое задание для самостоятельного выполнения Функции и процедуры. ч2/Задание 1.py
+++ Практическое задание для самостоятельного выполнения Функции и процедуры. ч2/Задание 1.py
@@ -1,3 +1,10 @@
 l1 = list(map(int, input().split()))
 l2 = list(map(int, input().split()))
+if len(l1) != len(l2):
+    if len(l1) > len(l2):
+        while len(l2) != len(l1):
+            l2.append(0)
+    if len(l2) > len(l1):
+        while len(l1) != len(l2):
+            l1.append(0)
 print(list(map(lambda x, y: x + y, l1, l2)))
