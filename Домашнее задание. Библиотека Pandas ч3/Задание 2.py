48b600c9ee3a066aa690f5ce3aaa617a79c90eee
ShiraSchool18 <145564330+ShiraSchool18@users.noreply.github.com>
2023-12-28 18:48:09 +0300

    Update Задание 2.py

--- Домашнее задание. Библиотека Pandas ч3/Задание 2.py
+++ Домашнее задание. Библиотека Pandas ч3/Задание 2.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('Customers.csv', sep=';')
-df.dropna(inplace=True)
+df.dropna(inplace=True)  # Удаление пустышок
 print(df.groupby('Profession')['Income'].mean())
